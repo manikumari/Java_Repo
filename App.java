1631062cddd71323cb441eb756296877f96f0bd0
Srinivasa Karri <srinivasakarri@Srinivasas-MacBook-Pro.local>
2022-06-26 11:35:20 -0700

    for loop practice

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
-public class App {
+public class App{
     public static void main(String[]args){
         System.out.println("Hello World!");
+        ForLoop_Practice.Print_Ten_Numbers();
     }
 }
---
+++ ForLoop_Practice.java
@@ -0,0 +1,11 @@
+public class ForLoop_Practice{
+    public static void Print_Ten_Numbers()
+    {
+        
+        for(int i=1; i<=10;i++)
+        {
+           System.out.println(i);
+        }
+        
+    }
+}
