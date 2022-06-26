91b4eee5adf8435cf11fc6a33cd6f0ad62669dfb
Srinivasa Karri <srinivasakarri@Srinivasas-MacBook-Pro.local>
2022-06-26 16:24:07 -0700

    Nested_ Loop_sample

--- ForLoop_Practice.java
+++ ForLoop_Practice.java
@@ -8,4 +8,14 @@ public class ForLoop_Practice{
         }
         
     }
+    public static void Nested_Loop_sample(char symbol,int rows, int columns)
+    {
+        for( int i=1; i<=rows; i++)
+        {
+            for(int k= 1; k<=columns;k++)
+                System.out.print(symbol);
+            System.out.println();
+        }
+
+    }
 }
--- WhileLoop_Practice.java
+++ WhileLoop_Practice.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class WhileLoop_Practice{
-    // public static void Check_if_Number_Is_Range(int number, int start, int end)
-    // {
-    //     while(number>=start && number<=end)
-    //     {
-    //         System.out.println(number+ "is in range.");
-    //         return;
-    //     }
+     public static void Check_if_Number_Is_Range(int number, int start, int end)
+    {
+        while(number>=start && number<=end)
+         {
+            System.out.println(number+ "is in range.");
+            return;
+        }
         
-    //     System.out.println(number+ "is not in range.");
+        System.out.println(number+ "is not in range.");
 
 
-    // }
+    }
     public static void Check_if_Number_Is_Range(int start, int end)
     {
 
@@ -24,15 +24,12 @@ public class WhileLoop_Practice{
         
             if(number>=start && number<=end)
             {
-                System.out.println(number+ "is in range."+start+ "");
+                System.out.println(number+ "is in range"+start+ "-"+ end);
                 break;
             }
            
         }
         
-        
-
-
     }
     
 }
