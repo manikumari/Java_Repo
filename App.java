5f5a20276ce76d29d72cedf260ed37885a534e34
Srinivasa Karri <srinivasakarri@Srinivasas-MacBook-Pro.local>
2022-06-26 15:48:59 -0700

    reading a value in whileloop

--- App.java
+++ App.java
@@ -3,6 +3,7 @@ public class App{
         System.out.println("Hello World!");
         //ForLoop_Practice.Print_Ten_Numbers();
         //Math_Util.Pow_Of_X(2,4);
-        WhileLoop_Practice.Check_if_Number_Is_Range(0,1,100);
+       // WhileLoop_Practice.Check_if_Number_Is_Range(0,1,100);
+        WhileLoop_Practice.Check_if_Number_Is_Range(1,100);
     }
 }
--- WhileLoop_Practice.java
+++ WhileLoop_Practice.java
@@ -1,13 +1,36 @@
+import java.util.Scanner;
+
 public class WhileLoop_Practice{
-    public static void Check_if_Number_Is_Range(int number, int start, int end)
+    // public static void Check_if_Number_Is_Range(int number, int start, int end)
+    // {
+    //     while(number>=start && number<=end)
+    //     {
+    //         System.out.println(number+ "is in range.");
+    //         return;
+    //     }
+        
+    //     System.out.println(number+ "is not in range.");
+
+
+    // }
+    public static void Check_if_Number_Is_Range(int start, int end)
     {
-        while(number>=start && number<=end)
+
+        while(true)
         {
-            System.out.println(number+ "is in range.");
-            return;
+            System.out.println("To enter this loop an integer value between the range" + start+"-"+end);
+            Scanner reader = new Scanner(System.in);
+            int number=reader.nextInt();
+        
+            if(number>=start && number<=end)
+            {
+                System.out.println(number+ "is in range."+start+ "");
+                break;
+            }
+           
         }
         
-        System.out.println(number+ "is not in range.");
+        
 
 
     }
